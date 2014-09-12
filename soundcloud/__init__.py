35e3d820168ae1042cf7bf3091a3285b92d55a4e
christheswiss <chris.luscher@gmx.de>
2014-09-12 15:16:05 +0200

    soundcloud basic setup

---
+++ soundcloud/__init__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+"""
+soundcloud
+~~~~~~~~~~
+
+*soundcloud* is a wrapper, written in Python, for Soundcloud.
+
+To find out more about Soundcloud, check out:
+http://www.soundcloud.com
+
+:copyright: (c) 2014 by Chris Luscher.
+:license: 2-clause BSD, see License.txt for more details
+"""
+
+__title__ = 'soundcloud'
+__version__ = '0.0.0'
+__author__ = 'Chris Luscher'
+__copyright__ = 'Copyright (c) 2014 Chris Luscher'
+__license__ = '2-clause BSD'
+
+import os
+
+import .soundcloud
---
+++ soundcloud/soundcloud.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+"""
+	soundcloud.soundcloud
+	~~~~~~~~~~~~~~~
+	This module implements the base of soundcloud.
+	
+	Created by Chris Luscher on 2014-09-12.
+	
+	:copyright: (c) 2014 by Chris Luscher
+	:license: 2-clause BSD, see License.txt for more details
+	"""
